d7198ef94c99f76334ae5df4053cb9c8d0a100dc
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-06-01 11:57:21 +0530

    Solution-1 Done

---
+++ Python/LOOPS/solutions/01_solution.py
@@ -0,0 +1,16 @@
+def main():
+
+    numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+    countPositive = 0
+
+    for number in numbers:
+
+        if (number > 0):
+
+            countPositive += 1
+
+    return countPositive
+
+
+# print(main())
