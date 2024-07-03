dc6fe1a358c29e5f244f0a78eef02292d0641f7e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-07-03 23:54:05 +0530

    solution-3 Done

---
+++ Python/LOOPS/solutions/03_solution.py
@@ -0,0 +1,13 @@
+
+def operate(NUMBER):
+
+    for num in range(1 , 11):
+
+        if (num == 5):
+            
+            continue
+
+        print(f"{NUMBER} X {num} = {num*NUMBER}")
+
+
+operate(4)
