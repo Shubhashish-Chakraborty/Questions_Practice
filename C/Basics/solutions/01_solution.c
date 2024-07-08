9442818f39d1006b77b1176c882b6161121e3ff5
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-07-08 12:48:55 +0530

    Solution-01 Done

---
+++ C/Basics/solutions/01_solution.c
@@ -0,0 +1,19 @@
+// Calculate area of Rectangle
+
+
+#include <stdio.h>
+
+int main() {
+
+    float LENGTH , BREADTH;
+
+    printf("Length Of Rectangle: ");
+    scanf("%f" , &LENGTH);
+
+    printf("Width Of Rectangle: ");
+    scanf("%f" , &BREADTH);
+
+    printf("The Area of Rectangle is ==> %f \n" , LENGTH * BREADTH);
+
+    return 0;
+}
