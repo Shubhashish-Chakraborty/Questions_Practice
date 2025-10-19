15ee0346fcdb203395130a698ea8591fb749f999
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-10-19 16:15:16 +0530

    length of an array and using vector

--- cpp/solutions/Arr_Len.cpp
+++ cpp/solutions/Arr_Len.cpp
@@ -3,7 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void usingVectors() {
+    vector<string> usernames = {"Aryan" , "Shubh", "Shahrukh"};
+
+    cout << "{ ";
+    for (int i = 0; i < usernames.size(); i++) {
+        cout << usernames[i] << " ";
+    }
+    cout << "}" << endl;
+
+}
+
 int main() {
+    int myArr[] = {12,14,15,2};
+
+    cout << "{ ";
+    for (int i = 0; i < sizeof(myArr)/sizeof(myArr[0]); i++) {
+        cout << myArr[i] << " ";
+    }
+    cout << "}" << endl;
     
+    usingVectors();
     return 0;
 }
